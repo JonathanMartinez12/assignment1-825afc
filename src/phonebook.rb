1d4c234c5147a8a7c355b25455f0be707770278d
Jonathan Martinez <jonmart1204@gmail.com>
2025-09-15 18:49:36 -0500

    hw1 Jonathan Martinez

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,45 @@
 class PhoneBook
-    def initialize
+  def initialize
+    @entries = []
+  end
+
+  def add(name, number, is_listed)
+    @entries.each do |entry|
+      return false if entry["name"] == name
     end
 
-    def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+    return false unless number.match?(/\A\d{3}-\d{3}-\d{4}\z/)
+
+    if is_listed
+      @entries.each do |entry| #learned that .each do iterates through a list of stuff 
+        if entry["number"] == number && entry["listed"] == true
+          return false # meaning not allowd 
+        end
+      end
     end
+
+    @entries << { "name" => name, "number" => number, "listed" => !!is_listed } 
+    true
+  end
+end
 
     def lookup(name)
-        raise Exception, "Not implemented"
+       @entries.each do |entry| #loops through it 
+        return entry["number"] if entry["name"] == name && entry["listed"] == true
+       end
     end
 
     def lookupByNum(number)
-        raise Exception, "Not implemented"
+        @entries.each do |entry|
+          return entry["name"] if entry["number"] == number && entry["listed"] == true
+        end
+        nil
     end
 
     def namesByAc(areacode)
-        raise Exception, "Not implemented"
+        @entries.each do |entry|
+          return entry["name"] if entry["number"].start_with?(areacode) && entry["listed"] == true
+        end
+        nil
     end
 end
--- src/warmup.rb
+++ src/warmup.rb
@@ -1,23 +1,74 @@
 def fib(n)
-    raise Exception, "Not Implemented"
-end
+    return [] if n == 0
+    return [0] if n == 1
+    arr = [0,1]
+    while arr.length < n
+        arr << arr[-1] + arr[-2]
+    end
+    arr
+
 
 def isPalindrome(n)
-    raise Exception, "Not Implemented"
+str = n.to_s  
+if str.reverse == str
+    return true
+else 
+    return false
+
 end
 
 def nthmax(n, a)
-    raise Exception, "Not Implemented"
+   return nil if n < 0
+   sortarr = a.sort
+   index = -1 - n
+   return nil if sortarr.empty? || index.abs > sortarr.length #we have to include the or operator because if the array is empty, index will be -1 and abs(-1) > 0 is true so it'll return nill 
+   return sortarr[index]
+   
+
+   sortarr[index]
+
 end
 
 def freq(s)
-    raise Exception, "Not Implemented"
+  return "" if s == ""
+  counts = {}
+  for i in 0...s.length
+    cha = s[i]
+    if counts.key?(cha)
+      counts[cha] = counts[cha] + 1
+    else
+      counts[cha] = 1
+    end
+  end
+  max_cha = nil
+  max_count = -1
+  counts.each do |cha, cnt|
+    if cnt > max_count
+      max_count = cnt
+      max_cha = cha
+    end
+  end
+  max_cha
 end
 
+
 def zipHash(arr1, arr2)
-    raise Exception, "Not Implemented"
+   return nil if arr1.length != arr2.length # I have this here in case the length of the arras don't match
+   final = {} #initializes my empty hash
+   for i in 0...arr1.length 
+    1 = arr1[i] # for the key
+    2 = arr2[i] 
+    final[1] = 2
+   end
+   final
 end
 
 def hashToArray(hash)
-    raise Exception, "Not Implemented"
+arr = []
+hash.each do |key, value|
+    pair = [key, value]
+    arr << pair
 end
+arr
+end
+
